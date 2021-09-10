bcf94f7d0badbf2d059853195b2a86836d9df62c
chaisengl <chaisengl@student.unimelb.edu.au>
2021-09-10 21:02:14 +1000

    test circleci 6

--- __tests__/sampleTest1.js
+++ __tests__/sampleTest1.js
@@ -2,4 +2,7 @@ describe("getting started with JEST", () => {
     test("this is just a JEST", () => {
         expect(123).toEqual(456);
     });
+    test("this is just a JEST2", () => {
+        expect(123).toEqual(123);
+    });
   }); 
