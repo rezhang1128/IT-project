aab71e85e92a392071cefbd7d2bdffe2f642609d
jiahern <jiahern@student.unimelb.edu.au>
2021-09-20 22:53:25 +0800

    union pull

--- controllers/unionControllers.js
+++ controllers/unionControllers.js
@@ -3,18 +3,25 @@ const Union = UnionModel.Union;
 const mongoose = require("mongoose");
 
 const testingAddUnion = async (req, res) => {
-    // var newUser = new User();
-    // newUser.firstName = "Vincent";
-    // newUser.lastName = "Gestio";
-    // newUser.email = "cust1@test.com";
-    // newUser.password = "123456";
-    // newUser.phoneNo = "12345678";
-    // newUser.save();
-    // console.log(newUser);
-}
+  var newUser = new User();
+  newUser.firstName = "Vincent";
+  newUser.lastName = "Gestio";
+  newUser.email = "cust1@test.com";
+  newUser.password = "123456";
+  newUser.phoneNo = "12345678";
+  newUser.save();
+  console.log(newUser);
+  res.send(newUser);
+};
 
-
+//Controller 2
+const getAllUnion = async (req, res) => {
+  let Unions = await Union.find({ userId: req.user._id }).lean();
+  console.log("Unions = " + Unions);
+  res.json(Unions);
+};
 
 module.exports = {
-    testingAddUnion
+  testingAddUnion,
+  getAllUnion,
 };
--- routes/unionRouters.js
+++ routes/unionRouters.js
@@ -1,17 +1,9 @@
 const express = require("express");
 const router = express.Router();
 // connect to food controller -- we will use the food controller to update the
-// favourites list. 
-const unionController = require('../controllers/unionControllers.js')
-
-
+// favourites list.
+const unionController = require("../controllers/unionControllers.js");
 
 router.get("/testing/addUnion", unionController.testingAddUnion);
-
-
-
-
-
-
 
 module.exports = router;
