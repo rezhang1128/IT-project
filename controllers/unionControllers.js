7cdc06150b29befac7d9e1ee6afd3e0afa442b64
rezhang1128 <ryenzhang1128@gmail.com>
2021-09-20 21:26:47 +1000

    union part

--- controllers/unionControllers.js
+++ controllers/unionControllers.js
@@ -1,20 +1,28 @@
 const UnionModel = require("../models/unionModels");
 const Union = UnionModel.Union;
 const mongoose = require("mongoose");
+let ObjectId = require("mongoose").Types.ObjectId;
 
 const testingAddUnion = async (req, res) => {
-    // var newUser = new User();
-    // newUser.firstName = "Vincent";
-    // newUser.lastName = "Gestio";
-    // newUser.email = "cust1@test.com";
-    // newUser.password = "123456";
-    // newUser.phoneNo = "12345678";
-    // newUser.save();
-    // console.log(newUser);
+    var newUser = new Union();
+    newUser.userId =  new ObjectId("6139e1cd8e40774fd8ac61ba");
+    newUser.name = "Alice";
+    newUser.linkages = [];
+    newUser.save();
+    console.log(newUser);
+    res.send(newUser)
+
+}
+
+const getAllUnion = async (req, res) => {
+    let unions = await Union.find({userId: req.user._id}).lean();
+    console.log("unions = "+ unions);
+    res.json(unions);
 }
 
 
-
 module.exports = {
-    testingAddUnion
+    
+    testingAddUnion,
+    getAllUnion
 };
--- routes/unionRouters.js
+++ routes/unionRouters.js
@@ -3,13 +3,15 @@ const router = express.Router();
 // connect to food controller -- we will use the food controller to update the
 // favourites list. 
 const unionController = require('../controllers/unionControllers.js')
-
+const passport = require('passport');
+require('../config/passport')(passport);
+const jwt = require('jsonwebtoken');
 
 
 router.get("/testing/addUnion", unionController.testingAddUnion);
 
 
-
+router.get("/",passport.authenticate('jwt', { session: false }), (req,res) => unionController.getAllUnion(req, res));
 
 
 
